36d006fb4554c82f758e8760e013f3be6b943cd2
alenderman12 <alexrothpino@gmail.com>
2025-05-07 22:15:20 -0300

    Clase 7

--- Rotiseria/dominio/rotiseria.js
+++ Rotiseria/dominio/rotiseria.js
@@ -1,15 +1,17 @@
+let elementos = [];
+
 function CargarValores(dia) {
     
 }
 
 function Inicio() {
     LimpiarCajas();
-
 }
 
 function LimpiarCajas() {
     document.getElementById("frm-entradas").reset();
     document.getElementById("nombre-entrada-1").focus();
+
     document.getElementById("imagen-entrada-muestra").style.display = "none";
     document.getElementById("etiqueta-muestra").style.display = "none";
 }
@@ -18,19 +20,42 @@ function AgregarEntrada() {
     let nombre1 = document.getElementById("nombre-entrada-1").value;
     let precio1 = document.getElementById("precio-entrada-1").value;
     let imagenLink = document.getElementById("imagen-entrada").value;
+    
+    if(imagenLink == "") {
+        alert("Ingrese imagen");
+        return;
+    }    
 
-    let lista = document.getElementById("lista").options;
-    let item = new Option("Nombre 1: " + nombre1 + " - Precio 1: " + precio1);
+    document.getElementById("imagen-entrada-muestra").style.display = "block";
+    document.getElementById("etiqueta-muestra").style.display = "block";
+    document.getElementById("imagen-entrada-muestra").src = imagenLink;
 
-
-    if(nombre1 != "" && precio1 != "" && precio1 >= 0) {
-        lista.add(item);
-        LimpiarCajas();
-        
-        if(imagenLink != "") {
-            document.getElementById("imagen-entrada-muestra").style.display = "block";
-            document.getElementById("etiqueta-muestra").style.display = "block";
-            document.getElementById("imagen-entrada-muestra").src = imagenLink;
-        }
+    if(nombre1 == "") {
+        alert("Ingrese nombre");
+        return;
     }
+    
+    if(precio1 == "") {
+        alert("Ingrese precio");
+        return;
+    }
+    
+    if(isNaN(precio1)) {
+        alert("El precio no puede contener letras");
+        return;
+    }
+
+    ListarEntradas(nombre1, precio1, imagenLink);
+
+    LimpiarCajas();
+}
+
+function ListarEntradas(nombre, precio, imagen) {
+    let elemento ='<tr>'+
+                    `<th>${nombre}</th>`+
+                    `<th>$${precio}</th>`+
+                    `<th><img src="${imagen}"></th>`+
+                '</tr>';
+
+    document.getElementById("tabla").innerHTML += elemento;
 }
